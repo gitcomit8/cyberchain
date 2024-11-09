82c39d8448d5a398282153a2f71c0086d49280df
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2024-11-09 19:23:54 +0530

    Add mining functionality

--- CyberChain.java
+++ CyberChain.java
@@ -3,11 +3,22 @@ import com.google.gson.GsonBuilder;
 
 public class CyberChain {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty = 1;
 
     public static void main(String[] args) {
         blockchain.add(new Block("First Block", "0"));
+        System.out.println("Trying to mine block 1...");
+        blockchain.get(0).mineBlock(difficulty);
+
         blockchain.add(new Block("Second Block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to mine block 2...");
+        blockchain.get(1).mineBlock(difficulty);
+
         blockchain.add(new Block("Third Block", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Trying to mine block 3...");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\n Blockchain is Valid: " + isChainValid());
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockchainJson);
@@ -16,6 +27,7 @@ public class CyberChain {
     public static boolean isChainValid() {
         Block currentBlock;
         Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
         for (int i = 1; i < blockchain.size(); i++) {
             currentBlock = blockchain.get(i);
@@ -28,6 +40,10 @@ public class CyberChain {
 
             if (!previousBlock.hash.equals(currentBlock.previousHash)) {
                 System.out.println("Previous Hashes not equal");
+                return false;
+            }
+            if (!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+                System.out.println("Block hasn't been mined");
                 return false;
             }
         }
