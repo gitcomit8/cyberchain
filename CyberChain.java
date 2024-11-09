6c407a7d4af2aab57b3b88ce0c64117e80a3d6e2
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2024-11-09 18:33:16 +0530

    Add chain validation method

--- CyberChain.java
+++ CyberChain.java
@@ -12,4 +12,25 @@ public class CyberChain {
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockchainJson);
     }
+
+    public static boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
+    }
 }
