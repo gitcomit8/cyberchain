75a731fe48ccad1d7c0d4c10e909c430062edc3e
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2024-11-10 00:15:38 +0530

    Add Key Pair Generation

--- src/cyberchain/wallet.java
+++ src/cyberchain/wallet.java
@@ -1,5 +1,29 @@
 package cyberchain;
 
-public class wallet {
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
+public class wallet {
+    public PrivateKey privateKey;
+    public PublicKey publicKey;
+
+    public Wallet(){
+        generateKeyPair();
+    }
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+
+            keyGen.initialize(ecSpec, random);
+            KeyPair keyPair = keyGen.generateKeyPair();
+
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
